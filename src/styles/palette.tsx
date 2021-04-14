562a9aeaca739c958ee55ecaf1ddb6478f5be831
Kamil Lewandowski <kamil.lewandowski@stxnext.pl>
2021-04-14 15:16:36 +0200

    typo

--- src/styles/palette.tsx
+++ src/styles/palette.tsx
@@ -1,7 +1,6 @@
 export const palette = {
   primary: '#58c5c4',
   secondary: '#39B54A',
-  mianBlue: '#08465c',
   mainBackground: '#0f1218',
   secondaryBackground: '#21272b',
   mainBlack: '#161518',
